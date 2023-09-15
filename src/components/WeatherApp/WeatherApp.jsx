149b051387ae4b5eccc501ceb7c9c598bb9752c0
Alejandro Leon <alejandroleortiz57@gmail.com>
2023-09-15 19:38:38 -0400

    finalizado fecth con useState

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -7,36 +7,40 @@ import rain_icon from '../assets/rain.png';
 import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
+import { useState } from 'react';
 
 export const WeatherApp = () => {
 
-    let api_key = "";
+    const api_key = "be4adac0927338d30895f60fe22f0a58";
+
+    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
 
     const search = async () => {
-        const element = document.getElementsByClassName("cityInput")
-        if (element[0].value === "") {
-            return 0;
+        if (!city) return;
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${api_key}`;
+
+        try {
+            const response = await fetch(url)
+            const data = await response.json();
+            setWeatherData(data)
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching weather data', error)
         }
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
-
-        let response = await fetch(url);
-        let data = response.json();
-        console.log(data);
-        const humidity = document.getElementsByClassName("humidity-percent")
-        const wind = document.getElementsByClassName("wind-rate")
-        const temperature = document.getElementsByClassName("weather-temp")
-        const location = document.getElementsByClassName("weather-location")
-
-        humidity[0].innerHTML = data.main.humidity;
-        wind[0].innerHTML = data.wind.speed;
-        temperature[0].innerHTML = data.main.temp;
-        location[0].innerHTML = data.name;
-    }
+    };
 
     return (
         <div className='container'>
             <div className="top-bar">
-                <input type="text" className="cityInput" placeholder='Search' />
+                <input
+                    type="text"
+                    className="cityInput"
+                    placeholder='Search'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
                 <div className="search-icon" onClick={() => { search() }}>
                     <img src={search_icon} alt="" />
                 </div>
@@ -44,14 +48,18 @@ export const WeatherApp = () => {
             <div className="weather-image">
                 <img src={cloud_icon} alt="" />
             </div>
-            <div className='weather-temp'>24°c</div>
-            <div className="weather-location">London</div>
+            <div className='weather-temp'>
+                {weatherData ? `${weatherData.main.temp}°c` : 'Loading...'}
+            </div>
+            <div className="weather-location">{weatherData ? `${weatherData.name}` : 'Loading...'}</div>
             <div className="data-container">
 
                 <div className="element">
                     <img src={humidity_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">64%</div>
+                        <div className="humidity-percent">
+                            {weatherData ? `${weatherData.main.humidity}%` : 'Loading...'}
+                        </div>
                         <div className="text">Humidity</div>
                     </div>
                 </div>
@@ -60,7 +68,9 @@ export const WeatherApp = () => {
                     <img src={wind_icon} alt="" className="icon" />
                     <div className="data">
                         <div className="wind-rate">18 km/h</div>
-                        <div className="text">Wind Speed</div>
+                        <div className="text">
+                            {weatherData ? `${weatherData.wind.speed} km/h` : 'Loading...'}
+                        </div>
                     </div>
                 </div>
 
