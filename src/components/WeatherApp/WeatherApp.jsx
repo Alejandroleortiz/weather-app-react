85b01dcdf100d935117822047fc8ae20fc542565
Alejandro Leon <alejandroleortiz57@gmail.com>
2023-09-15 19:39:58 -0400

    finalizado fecth con useState

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,17 +1,17 @@
 import './WeatherApp.css'
 import search_icon from '../assets/search.png';
-import clear_icon from '../assets/clear.png';
+// import clear_icon from '../assets/clear.png';
 import cloud_icon from '../assets/cloud.png';
-import drizzle_icon from '../assets/drizzle.png';
-import rain_icon from '../assets/rain.png';
-import snow_icon from '../assets/snow.png';
+// import drizzle_icon from '../assets/drizzle.png';
+// import rain_icon from '../assets/rain.png';
+// import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
 import { useState } from 'react';
 
 export const WeatherApp = () => {
 
-    const api_key = "be4adac0927338d30895f60fe22f0a58";
+    const api_key = "";
 
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState(null);
